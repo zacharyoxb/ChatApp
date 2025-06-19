f169d4015b926686a19d9ad30c6b4a99345d8d05
zacharyoxb <oxboroughzachary@protonmail.com>
2025-06-19 19:08:10 +0100

    homepage done

--- frontend/src/routes/home.tsx
+++ frontend/src/routes/home.tsx
@@ -1,24 +1,45 @@
-import './home.css'
+import { useState } from 'react';
+import './css/home.css'
 import { Link } from 'react-router';
 
 function Home() {
+  const [hovered, setHovered] = useState(false);
 
   return (
-    <div id='parent'>
-      <h1>
-        ChatApp: Basic Group Messaging Service
-      </h1>
-      <div className='Buttons'>
-        <Link className='button-link' to="/login">
+    <div id="parent">
+      <h1>ChatApp: Basic Group Messaging Service</h1>
+      <div id="link-buttons">
+        <Link
+          className="link-button"
+          to="/login"
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+          style={{
+            backgroundColor: hovered ? '#333' : '#eee',
+            color: hovered ? 'white' : 'black',
+            padding: '0.5rem 1rem',
+            borderRadius: '4px',
+            textDecoration: 'none',
+            marginRight: '1rem'
+          }}
+        >
           Login
         </Link>
 
-        <Link className='button-link' to="/signup">
+        <Link className="link-button" to="/signup"
+          style={{
+            backgroundColor: '#eee',
+            color: 'black',
+            padding: '0.5rem 1rem',
+            borderRadius: '4px',
+            textDecoration: 'none'
+          }}
+        >
           Sign Up
         </Link>
       </div>
     </div>
-  )
+  );
 }
 
 export default Home
