22ab1813c6bcb820dd51e3e668e25396d42c72ba
zacharyoxb <oxboroughzachary@protonmail.com>
2025-07-26 15:08:19 +0100

    less nesting

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -3,7 +3,6 @@ Functions for editing and accessing databases
 """
 
 import logging
-import sys
 import time
 import mysql.connector
 from mysql.connector import errorcode
@@ -12,34 +11,33 @@ from mysql.connector import errorcode
 def connect_to_mysql(config, logger: logging.Logger, attempts=3, delay=2):
     """ Attempts connection to mysql instance """
     attempt = 1
-    # Implement a reconnection routine
     while attempt < attempts + 1:
         try:
             return mysql.connector.connect(**config)
         except (mysql.connector.Error, IOError) as err:
-            if attempts is attempt:
-                # Attempts to reconnect failed; returning None
-                logger.info("Failed to connect, exiting without a connection: %s", err)
-                return None
-            logger.info(
-                "Connection failed: %s. Retrying (%d/%d)...",
-                err,
-                attempt,
-                attempts-1,
-            )
-            # progressive reconnect delay
             time.sleep(delay ** attempt)
             attempt += 1
-    return None
+            if attempt is not attempts:
+                logger.info(
+                    "Connection failed: %s. Retrying (%d/%d)...",
+                    err,
+                    attempt,
+                    attempts-1,
+                )
 
-def connect_to_db(cnx: mysql.connector.MySQLConnection, db_name):
+    logger.info("Failed to connect, exiting without a connection: %s", err)
+    raise err
+
+def connect_to_db(cnx: mysql.connector.MySQLConnection, logger: logging.Logger, db_name: str):
     """ Connects to a database """
     with cnx.cursor() as cursor:
         try:
             cursor.execute(f"USE {db_name}")
+            cnx.database = db_name
+            return
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_BAD_DB_ERROR:
                 cursor.execute(f"CREATE DATABASE {db_name} DEFAULT CHARACTER SET 'utf8'")
             else:
-                sys.exit(1)
-        cnx.database = db_name
+                logger.info("Failed to connect to database, exiting without connection: %s", err)
+                raise err
--- backend/app/database/user_group_db.py
+++ backend/app/database/user_group_db.py
@@ -4,6 +4,7 @@ import os
 
 from connection import connect_to_mysql, connect_to_db
 from dotenv import load_dotenv
+import mysql.connector
 
 load_dotenv()
 DB_NAME = "UserGroupRelations"
@@ -18,61 +19,60 @@ config = {
   'raise_on_warnings': True
 }
 
-TABLES = {}
-""" Tables establishing relationship between Users and Groups """
+# Dictionary to store table creation SQLs
+TABLES = {
+    'users': (
+        "CREATE TABLE IF NOT EXISTS `users` ("
+        " `user_id` BINARY(16),"
+        " `user_name` varchar(18) NOT NULL,"
+        " PRIMARY KEY (`user_id`)"
+        ")"
+    ),
+    'groups': (
+        "CREATE TABLE IF NOT EXISTS `groups` ("
+        " `group_id` BINARY(16),"
+        " `group_name` varchar(30) NOT NULL,"
+        " PRIMARY KEY (`group_id`)"
+        ")"
+    ),
+    'users_in_groups': (
+        "CREATE TABLE IF NOT EXISTS `users_in_groups` ("
+        " `user_id` BINARY(16),"
+        " `group_id` BINARY(16),"
+        " `perms` ENUM('Owner','Admin','Member') NOT NULL,"
+        " PRIMARY KEY (`user_id`, `group_id`),"
+        " FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,"
+        " FOREIGN KEY (`group_id`) REFERENCES `groups` (`group_id`) ON DELETE CASCADE"
+        ")"
+    )
+}
 
-TABLES['users'] = (
-    "CREATE TABLE IF NOT EXISTS `users` ("
-    " `user_id` BINARY(16),"
-    " `user_name` varchar(18) NOT NULL,"
-    " PRIMARY KEY (`user_id`)"
-    ")"
-)
+IS_INIT = False
 
-TABLES['groups'] = (
-    "CREATE TABLE IF NOT EXISTS `groups` ("
-    " `group_id` BINARY(16),"
-    " `group_name` varchar(30) NOT NULL,"
-    " PRIMARY KEY (`group_id`)"
-    ")"
-)
+def init_db():
+    """ Initialises the database if not initialised, connects to db"""
+    # Set up logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    # Log to console
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    # get path to log file
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    log_path = os.path.join(script_dir, 'logs', 'user-group-relations.log')
+    # Also log to a file
+    file_handler = logging.FileHandler(log_path)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
-TABLES['users_in_groups'] = (
-    "CREATE TABLE IF NOT EXISTS `users_in_groups` ("
-    " `user_id` BINARY(16),"
-    " `group_id` BINARY(16),"
-    " `is_admin` BOOLEAN NOT NULL,"
-    " PRIMARY KEY (`user_id`, `group_id`),"
-    " FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,"
-    " FOREIGN KEY (`group_id`) REFERENCES `groups` (`group_id`) ON DELETE CASCADE"
-    ")"
-)
-
-# Set up logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-
-# Log to console
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
-# get path to log file
-script_dir = os.path.dirname(os.path.abspath(__file__))
-log_path = os.path.join(script_dir, 'logs', 'user-group-relations.log')
-
-# Also log to a file
-file_handler = logging.FileHandler(log_path)
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-
-# connects to server, initialises db if doesn't exist
-cnx = connect_to_mysql(config, logger, 10)
-
-if cnx and cnx.is_connected():
-    connect_to_db(cnx, DB_NAME)
-    with cnx.cursor() as cursor:
-        for _, table_desc in TABLES.items():
-            cursor.execute(table_desc)
+    try:
+        cnx = connect_to_mysql(config, logger, 10)
+        connect_to_db(cnx, logger, DB_NAME)
+        with cnx.cursor() as cursor:
+            for _, table_desc in TABLES.items():
+                cursor.execute(table_desc)
+    except mysql.connector.Error as err:
+        logger.error("Error during database initialisation: %s", err)
     
