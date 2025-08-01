2d7ebe2c6894179947e9ca44b2eac0d2e039788a
zacharyoxb <oxboroughzachary@protonmail.com>
2025-08-01 15:38:25 +0100

    connects to db

--- backend/app/database/db_connection.py
+++ backend/app/database/db_connection.py
@@ -1,38 +1,35 @@
+""" Connects to AWS database """
 import os
 from dotenv import load_dotenv
-import boto3
+
+import mysql.connector
+from mysql.connector import Error
 
 load_dotenv()
 
-AWS_REGION_NAME=os.getenv('AWS_ENDPOINT')
-AWS_ACCESS_KEY_ID=os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY=os.getenv('AWS_SECRET_ACCESS_KEY')
+db_user = os.getenv('DB_USER')
+db_pass = os.getenv('DB_PASS')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+db_name = os.getenv('DB_NAME')
+
+config = {
+  'user': db_user,
+  'password': db_pass,
+  'host': db_host,
+  'port': db_port,
+  'database': db_name,
+  'raise_on_warnings': True
+}
+
+try:
+    connection = mysql.connector.connect(**config)
 
 
-# Create RDS client
-rds = boto3.client("rds", region_name="eu-north-1", aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+except Error as e:
+    print("Connection error:", e)
 
-# Create a paginator for the describe_db_clusters operation
-paginator = rds.get_paginator("describe_db_clusters")
-
-# Use the paginator to get a list of DB clusters
-response_iterator = paginator.paginate(
-    PaginationConfig={
-        "PageSize": 50,  # Adjust PageSize as needed
-        "StartingToken": None,
-    }
-)
-
-# Iterate through the pages of the response
-clusters_found = False
-for page in response_iterator:
-    if "DBClusters" in page and page["DBClusters"]:
-        clusters_found = True
-        print("Here are your RDS Aurora clusters:")
-        for cluster in page["DBClusters"]:
-            print(
-                f"Cluster ID: {cluster['DBClusterIdentifier']}, Engine: {cluster['Engine']}"
-            )
-
-if not clusters_found:
-    print("No clusters found!")
+finally:
+    if 'connection' in locals() and connection.is_connected():
+        connection.close()
+        print("Connection closed")
