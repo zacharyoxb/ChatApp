b6e2426b1b2c2b1a4ba4936105072ccc2c26cf58
zacharyoxb <oxboroughzachary@protonmail.com>
2025-08-02 13:20:46 +0100

    added cert to frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync('certs/vite-server.key'),
+      cert: fs.readFileSync('certs/vite-server.crt')
+    },
+    port: 3000,
+  },
   plugins: [react()],
 })
