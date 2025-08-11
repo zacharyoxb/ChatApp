70c5e9d068290051b3ab15838558c876b58da089
zacharyoxb <oxboroughzachary@protonmail.com>
2025-08-11 21:39:22 +0100

    session cookie only last 1 hour

--- backend/app/utils/cookies.py
+++ backend/app/utils/cookies.py
@@ -1,13 +1,15 @@
 """ Standardises cookie defaults """
+from datetime import datetime, timedelta
 from fastapi import Response
 
 def set_session_cookie(response: Response, session_id: str):
     """ Makes a session cookie, adds to response"""
+    expiry_time = datetime.now() + timedelta(hours=1)
     response.set_cookie(
         key="session_id",
         value=session_id,
         httponly=True,
         secure=True,
         samesite="none",
-        max_age=86400 # 1 day
+        expires=expiry_time,
     )
