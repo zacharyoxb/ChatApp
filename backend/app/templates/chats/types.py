f1191d02958cfde2fd5afad58d12a3b93f7eba83
zacharyoxb <oxboroughzachary@protonmail.com>
2025-11-10 15:47:19 +0000

    Types note

--- backend/app/templates/chats/types.py
+++ backend/app/templates/chats/types.py
@@ -1,4 +1,8 @@
+""" All non-request/response types related to chats"""
 from enum import Enum
+
+# we probably wanna move this at some point as it
+# probably will be used for requests/responses
 
 
 class Role(Enum):
